cfbdc9d972f54f30d7f7b3ab2bc5b347640befd9
Katyliu <1270520801@qq.com>
2022-09-05 13:53:24 +0800

    生成用户认证

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,32 @@ Route::get('/', 'PagesController@root')->name('root');
 
 //不需要
 //Route::get('/', [\App\Http\Controllers\PagesController::class, 'root'])->name('root');
+//Auth::routes();
+//Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+//用户身份验证相关的路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+//用户注册相关路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+//密码重置相关路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+//再次确认密码
+Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirm')->name('password.confirm');
+Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
+
+//Email 认证相关路由
+Route::get('email/verity', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
