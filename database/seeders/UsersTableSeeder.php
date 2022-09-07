1dcbcbeb525b8154acb2170d16daf7dec09d9fcd
Katyliu <1270520801@qq.com>
2022-09-07 13:30:18 +0800

    填充用户和话题数据

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //生成数据集合
+        User::factory()->count(10)->create();
+
+    }
+}
